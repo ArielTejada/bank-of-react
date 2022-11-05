0229dd2690bf992e3f73be6eb16ec21bdaf28efc
Ariel Tejada <85589271+ArielTejada@users.noreply.github.com>
2022-11-05 19:49:07 -0400

    Updates

---
+++ precache-manifest.aca87b679694c790584b3615dffcc1a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c124d7ac1e54a5004cb",
+    "url": "/bank-of-react/static/css/main.24b90f17.chunk.css"
+  },
+  {
+    "revision": "1c124d7ac1e54a5004cb",
+    "url": "/bank-of-react/static/js/main.1c124d7a.chunk.js"
+  },
+  {
+    "revision": "de53d44062204e8b07c9",
+    "url": "/bank-of-react/static/js/1.de53d440.chunk.js"
+  },
+  {
+    "revision": "3dba0d12b1f781f2b8d8",
+    "url": "/bank-of-react/static/js/2.3dba0d12.chunk.js"
+  },
+  {
+    "revision": "4de490c2405147fa0376",
+    "url": "/bank-of-react/static/js/runtime~main.4de490c2.js"
+  },
+  {
+    "revision": "923325415e22e229c001e5ee587f6e27",
+    "url": "/bank-of-react/index.html"
+  }
+];

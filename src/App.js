8a6581f97054ea80bc237a7eeffa9c4825a9bace
Ariel Tejada <85589271+ArielTejada@users.noreply.github.com>
2022-11-06 22:14:45 -0500

    made some changes to credits.js

--- src/App.js
+++ src/App.js
@@ -15,19 +15,23 @@ import UserProfile from './components/UserProfile';
 import LogIn from './components/Login';
 import Credits from './components/Credits';
 import Debits from './components/Debits';
+import axios from 'axios';
+import AccountBalance from './components/AccountBalance';
 
 class App extends Component {
   constructor() {  // Create and initialize state
     super(); 
     this.state = {
-      accountBalance: 1234567.89,
-      debitList: [],
+      accountBalance: 0,
       credits: [],
       debits: [],
+      creditsTotal: 0,
+      debitsTotal: 0,
       currentUser: {
         userName: 'Joe Smith',
         memberSince: '11/22/99',
-      }
+      },
+      date: ''
     }
   }
 
@@ -38,26 +42,94 @@ class App extends Component {
     this.setState({currentUser: newUser})
   }
 
-  addCredit = () => {
-    
+  addCredit = (credit) => {
+    let newCredits = this.state.credits;
+    newCredits.push(credit);
+    this.setState({credits: newCredits});
+    let newAccountBalance = this.state.accountBalance;
+    newAccountBalance += credit.amount;
+    this.setState({accountBalance: newAccountBalance});
   }
 
   addDebit = () => {
     
   }
 
-  // Create Routes and React elements to be rendered using React components
+  sumCredits = () => {
+    let sumCredits = 0.0;
+    this.state.credits.forEach((credit) => sumCredits += credit.amount);
+    this.setState({creditsTotal: sumCredits})
+    this.setState({accountBalance: this.state.accountBalance + sumCredits})
+  }
+
+  sumDebits = () => {
+    let sumDebits = 0.0;
+    this.state.debits.forEach((debit) => sumDebits += debit.amount);
+    this.setState({debitsTotal: sumDebits});
+    this.setState({accountBalance: this.state.accountBalance - sumDebits})
+  }
+
+  async componentDidMount() {
+    let creditsAPI = 'https://moj-api.herokuapp.com/credits';  
+    try { 
+      let credits_response = await axios.get(creditsAPI);
+      console.log(credits_response);  
+      this.setState({credits: credits_response.data}); 
+      this.setState({date: new Date().toISOString()})
+      this.sumCredits();
+      } 
+      catch (error) {  
+        if (error.credits_response) {
+          console.log(error.credits_response.data);  
+          console.log(error.credits_response.status);  
+        }    
+    }
+  }  
+
+  async componentDidMount() { 
+    let debitsAPI = 'https://moj-api.herokuapp.com/debits'
+    try { 
+      let debits_response = await axios.get(debitsAPI);
+      console.log(debits_response);  
+      this.setState({debits: debits_response.data}); 
+      this.setState({date: new Date().toISOString()})
+      this.sumDebits();
+      } 
+      catch (error) {  
+        if (error.debits_response) {
+          console.log(error.debits_response.data); 
+          console.log(error.debits_response.status);  
+        }      
+    }
+  }  
+
   render() {  
-    // Create React elements and pass input props to components
+
+    // let total = this.state.creditsTotal - this.state.debitsTotal;
+    // this.setState({accountBalance: total.toFixed(2)})
+
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance} />);
     const UserProfileComponent = () => (
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
     );
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
-    const DebitsComponent = () => (<Debits debits={this.state.debitList} addDebit={this.addDebit}/>) 
-    const CreditsComponent = () => (<Credits addCredit={this.addCredit}/>)
+    
+    const DebitsComponent = () => (
+      <Debits 
+        debits={this.state.debits} 
+        creditsTotal={this.state.creditsTotal} 
+        debitsTotal={this.state.creditsTotal} 
+        addDebit={this.addDebit} 
+        balance={this.state.accountBalance} updateAccountBalance={this.updateAccountBalance} />) 
 
-    // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
+    const CreditsComponent = () => (
+      <Credits 
+        credits={this.state.credits} 
+        creditsTotal={this.state.creditsTotal} 
+        debitsTotal={this.state.creditsTotal} 
+        addCredit={this.addCredit} 
+        balance={this.state.accountBalance} updateAccountBalance={this.updateAccountBalance} />)
+
     return (
       <Router basename="/bank-of-react">
         <div>
